04b9709d174c4923ca483dafea4b9ac811e6fc95
tmdbts <tbrancosilva@hotmail.com>
2020-12-11 12:16:21 +0000

    better named 'wifis'

--- windows/main.py
+++ windows/main.py
@@ -2,9 +2,9 @@ import subprocess
 
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 
-wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
+wifi_list = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
 
-for wifi in wifis:
+for wifi in wifi_list:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', wifi, 'key=clear']).decode('utf-8').split('\n')
     results = [line.split(':')[1][1:-1] for line in results if 'Key Content' in line]
     
